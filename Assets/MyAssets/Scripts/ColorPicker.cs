ba5b84c1e0fe0cb828bc23d9bc2f14bca1f12a19
mportog <matheus.garciag@t-systems.com.br>
2019-08-06 13:56:12 -0300

    initial commit c/ color panel ok

---
+++ Assets/MyAssets/Scripts/ColorPicker.cs
@@ -0,0 +1,40 @@
+﻿using HoloToolkit.Examples.InteractiveElements;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+
+    public MeshRenderer mesh;//mesh MAT que vou alterar   
+    public GameObject colorChild;//pai das caixas de cor
+    public SliderGestureControl grad;
+
+    private Material originalColor;
+    private Material novoMat;//material que selecionei
+    void OnEnable()
+    {
+        originalColor = mesh.material;
+        novoMat = mesh.material;
+    }
+    public void SelectedMaterial(int novo)
+    {
+        novoMat = colorChild.transform.GetChild(novo).GetComponent<MeshRenderer>().material;
+        mesh.material = novoMat;
+    }
+    public void ChangeAlpha()
+    {
+        mesh.material.color = new Color(novoMat.color.r, novoMat.color.g, novoMat.color.b, grad.SliderValue);
+    }
+    public void ResetarCor()
+    {
+        novoMat = originalColor;
+        mesh.material = originalColor;
+        gameObject.SetActive(false);
+    }
+    public void SalvarCor()
+    {
+        mesh.material = novoMat;
+        gameObject.SetActive(false);
+    }
+}
